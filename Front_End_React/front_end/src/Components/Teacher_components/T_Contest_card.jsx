48436b4773276cb31b248f8728371a29e8253571
vraj72 <virajtandel72@gmail.com>
2023-12-28 15:23:04 +0530

    teacher-dashboard done with hard core values

---
+++ Front_End_React/front_end/src/Components/Teacher_components/T_Contest_card.jsx
@@ -0,0 +1,26 @@
+import { Button, Card, Container } from "react-bootstrap";
+import { useNavigate } from "react-router-dom";
+
+export function T_Contest_card(props){
+
+    const navigate = useNavigate();
+    function seeAttemptPage() {
+        navigate(`/teacher-see-attempts?contest_id=${props.ID}`)
+    }
+
+    return(
+        <Card className="mt-3 "  text="muted" >
+            <Card.Header  as="h5">Contest ID : {props.ID}</Card.Header>
+            <Card.Body>
+                <Card.Title>Title : {props.title}</Card.Title>
+                <Card.Text>
+                    <h6>Description : </h6>{props.description} <hr></hr><br/>
+                    <h6>Start Time : {props.start_time} </h6>
+                    <h6>End Time : {props.end_time} </h6>
+                </Card.Text>
+                <Button onClick={seeAttemptPage} variant="primary">See Results</Button>
+            </Card.Body>
+            <Card.Footer className="text-muted">Created AT : {props.created_at}</Card.Footer>
+        </Card>
+    );
+}
--- Front_End_React/front_end/src/Components/Teacher_components/T_DashBoard.jsx
+++ Front_End_React/front_end/src/Components/Teacher_components/T_DashBoard.jsx
@@ -1,9 +1,86 @@
+import { Button, Card, Col, Container, Row } from "react-bootstrap";
 import { THeader } from "./THeader";
+import { useState } from "react";
+import { T_Contest_card } from "./T_Contest_card";
+
 
 export function T_DashBoard(props){
+    //dummy data as of now
+    const[contestList, setContestList] = useState( [
+            {
+              "contest_id" : 1, 
+              "teacher_id" : 1  ,
+              "title" :  "title1", 
+              "topic" :  "String", 
+              "description" : "title1title1title1title1title1title1title1title1title1title1title1title1title1title1title1title1title1" ,
+              "start_time" :  "2023-10-31 15:30:45", 
+              "end_time" :  "2023-12-31 15:30:45", 
+              "created_at" :  "2023-10-31 15:30:45"
+            },
+          {
+              "contest_id" : 2, 
+              "teacher_id" : 1  ,
+              "title" :  "title1title1title1", 
+              "topic" :  "Array", 
+              "description" : "title1title1title1title1title1title1title1title1title1title1title1title1title1title1title1title1title1title1title1title1title1title1title1title1title1title1title1title1title1title1title1title1title1title1title1title1title1title1title1title1title1title1title1title1title1title1" ,
+              "start_time" :  "2023-12-31 15:30:45", 
+              "end_time" :  "2023-12-31 15:30:45", 
+              "created_at" :  "2023-12-31 15:30:45"
+            },
+          {
+              "contest_id" : 3, 
+              "teacher_id" : 1  ,
+              "title" :  "title1title1", 
+              "topic" :  "Number", 
+              "description" : "title1title1title1title1title1title1title1title1title1title1title1title1title1title1title1title1title1title1title1" ,
+              "start_time" :  "2023-12-31 15:30:45", 
+              "end_time" :  "2023-12-31 15:30:45", 
+              "created_at" :  "2023-12-31 15:30:45"
+            },
+          {
+              "contest_id" : 4, 
+              "teacher_id" : 1  ,
+              "title" :  "title1", 
+              "topic" :  "Array", 
+              "description" : "title1title1title1title1title1title1title1title1title1title1title1title1title1title1title1title1title1title1title1title1title1title1title1title1title1title1title1title1title1title1title1title1title1title1title1title1title1title1title1title1title1title1title1title1title1title1title1title1title1title1title1title1title1title1title1title1title1title1title1title1title1title1title1title1title1title1title1title1title1title1title1title1title1title1" ,
+              "start_time" :  "2023-12-31 15:30:45", 
+              "end_time" :  "2023-12-31 15:30:45", 
+              "created_at" :  "2023-12-31 15:30:45"
+            },
+          {
+              "contest_id" : 5, 
+              "teacher_id" : 1  ,
+              "title" :  "title1title1title1title1", 
+              "topic" :  "Number", 
+              "description" : "title1title1title1title1title1title1title1title1title1title1title1title1title1title1title1title1title1title1title1title1title1title1title1title1title1title1title1title1title1title1title1title1title1title1title1title1title1title1title1title1title1title1title1title1title1title1title1" ,
+              "start_time" :  "2023-12-31 15:30:45", 
+              "end_time" :  "2023-12-31 15:30:45", 
+              "created_at" :  "2023-12-31 15:30:45"
+            }
+        ]);
+
+
+      
     return(
         <>
-            <THeader text="Teacher DashBoard Page Edited by Rushikesh"></THeader>
+            <THeader text="Teacher DashBoard Page "></THeader>
+
+            <Container>
+                <Row xs={1} md={3} className="g-4">
+                    {contestList.map((element) => (
+                        <Col key={element.contest_id}> 
+                            <T_Contest_card title={element.title} 
+                                description={element.description} 
+                                topic={element.topic}
+                                ID={element.contest_id}
+                                start_time={element.start_time}
+                                end_time={element.end_time}
+                                created_at={element.created_at}>
+                            </T_Contest_card>
+                        </Col>
+                    ))}
+                </Row>
+            </Container>
         </>
     )
 }
