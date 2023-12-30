e98f8e974c87bddc560e08394f455abfbbbe58b3
vraj72 <virajtandel72@gmail.com>
2023-12-31 02:28:43 +0530

    Teacher API done

--- Back_End_SpringBoot/Backend/src/main/java/com/project/repository/ProblemRepository.java
+++ Back_End_SpringBoot/Backend/src/main/java/com/project/repository/ProblemRepository.java
@@ -4,9 +4,10 @@ import java.util.List;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import com.project.entity.Contest;
 import com.project.entity.Problem;
 
 public interface ProblemRepository extends JpaRepository< Problem, Integer> {
 	
-	    
+	 public List<Problem> findAllByContest( Contest contest);   
 }
