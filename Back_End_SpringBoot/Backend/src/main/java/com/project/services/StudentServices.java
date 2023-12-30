39338bf21cb06c35778fcfb7fd903e38575d936f
RushikeshJagdale10 <rushikeshjagdale10@gmail.com>
2023-12-31 03:29:20 +0530

    API's created for "studentLogin', "getProblemList", "getProblemDetails"

--- Back_End_SpringBoot/Backend/src/main/java/com/project/services/StudentServices.java
+++ Back_End_SpringBoot/Backend/src/main/java/com/project/services/StudentServices.java
@@ -1,5 +1,6 @@
 package com.project.services;
 
+import java.util.List;
 import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
@@ -8,8 +9,11 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Service;
 
 import com.project.dto.StudentLoginStatus;
-
+import com.project.entity.Contest;
+import com.project.entity.Problem;
 import com.project.entity.Student;
+import com.project.repository.ContestRepository;
+import com.project.repository.ProblemRepository;
 import com.project.repository.StudentRepository;
 
 @Service
@@ -18,7 +22,11 @@ public class StudentServices {
 	@Autowired
 	private StudentRepository studentRepository;
 	
+	@Autowired
+	private ContestRepository contestRepository;
 	
+	@Autowired
+    private ProblemRepository problemRepository;
 	
 	public ResponseEntity<StudentLoginStatus> studentLogin(Student student){
 		
@@ -41,5 +49,26 @@ public class StudentServices {
 		}
 		
 	}
+	
+	public ResponseEntity<List<Problem>> getProblemsByContestId(int contestId) {
+        Optional<Contest> contestOptional = contestRepository.findById(contestId);
+
+        if (contestOptional.isPresent()) {
+            Contest contest = contestOptional.get();
+            List<Problem> problems = problemRepository.findByContest(contest);
+
+            if (!problems.isEmpty()) {
+                return new ResponseEntity<>(problems, HttpStatus.ACCEPTED);
+            } else {
+                // No problems found for the given contest
+                return new ResponseEntity<>(HttpStatus.BAD_REQUEST);
+            }
+        } else {
+            // Contest not found
+            return new ResponseEntity<>(HttpStatus.BAD_REQUEST);
+        }
+    }
+	
+	
 
 }
