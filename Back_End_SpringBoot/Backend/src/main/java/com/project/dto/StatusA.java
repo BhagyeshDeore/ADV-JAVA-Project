14871b635984a7f07b0b7ceb50389b4cca113bea
ankitapareek03 <ankitacdac23@gmail.com>
2023-12-31 16:06:49 +0530

    admin API(2)

---
+++ Back_End_SpringBoot/Backend/src/main/java/com/project/dto/StatusA.java
@@ -0,0 +1,20 @@
+package com.project.dto;
+
+public class StatusA {
+	private boolean status;
+	private String message;
+	
+	public boolean isStatus() {
+		return status;
+	}
+	public void setStatus(boolean status) {
+		this.status = status;
+	}
+	public String getMessage() {
+		return message;
+	}
+	public void setMessage(String message) {
+		this.message = message;
+	}
+	
+}
--- Back_End_SpringBoot/Backend/src/main/java/com/project/services/AdminServices.java
+++ Back_End_SpringBoot/Backend/src/main/java/com/project/services/AdminServices.java
@@ -1,14 +1,58 @@
 package com.project.services;
 
+import java.util.List;
+
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
-import com.project.repository.AdminRepository;
+import com.project.dto.StatusA;
+import com.project.entity.Teacher;
+import com.project.repository.TeacherRepository;
 
 @Service
 public class AdminServices {
-	
 	@Autowired
-	private AdminRepository adminRepository;
+	private TeacherRepository teacherRepository;
+	
+	
+	
+	
+		
+	//Get All teacher List at Admin side	
+		
+	public List<Teacher> getTeacherList(  int adminId ) {
+		
+		System.out.println("List of all teachers :");
+		return teacherRepository.findAll();
+		
+		}
+	
+	//create Contest
+//	public StatusT createContest( Contest contest,  int teacherId ) {
+//		
+//		Optional<Teacher> foundTeacher = teacherRepository.findById(teacherId);
+//		contest.setTeacher(foundTeacher.get());
+//		contestRepository.save(contest);
+//		StatusT status = new StatusT();
+//		status.setMessage("Contest Created!");
+//		status.setStatus(true);
+//		
+//		return status;
+//	}
+
+
+//Register New Teacher
+	public StatusA registerTeacher(Teacher teacher) {
+		teacherRepository.save(teacher);
+		StatusA status =new StatusA();
+		status.setMessage("New Teacher Registered Successfully");
+		status.setStatus(true);
+		return status;
+	}
+
+		
+		
+	
 
 }
