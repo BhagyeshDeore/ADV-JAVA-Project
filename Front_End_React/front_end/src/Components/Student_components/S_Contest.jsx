96ed05b50bfb2a30f22e701479355009d9036f0a
RushikeshJagdale10 <rushikeshjagdale10@gmail.com>
2023-12-31 23:23:39 +0530

    API created for see problems

--- Front_End_React/front_end/src/Components/Student_components/S_Contest.jsx
+++ Front_End_React/front_end/src/Components/Student_components/S_Contest.jsx
@@ -1,11 +1,12 @@
-// src/S_Contest.jsx
-
-import React, { useState } from "react";
+import React, { useEffect, useState } from "react";
 import { Container, Row, Col, Form, Button } from "react-bootstrap";
 import { THeader } from "../Teacher_components/THeader";
-import { Link, useParams } from "react-router-dom";
+import { Link, useNavigate, useParams } from "react-router-dom";
+import { getProblems } from "../../Services/Teacher_services/Teacher_APIs";
 
-const S_Contest = () => {
+const S_Contest = (props) => {
+  const [problemData, setProblemData] = useState([]);
+  const params = useParams();
   const [filter, setFilter] = useState({
     easy: true,
     medium: true,
@@ -21,112 +22,37 @@ const S_Contest = () => {
     }));
   };
 
-  const problemsData = [
-    {
-      problem_id: 1,
-      contest_id: 1,
-      title: "java-if-else",
-      problem_statement:
-        "In this challenge, we test your knowledge of using if-else conditional statements to automate decision-making processes.",
-      explanation:
-        "Given an integer,  , perform the following conditional actions:      If   is odd, print Weird If is even and in the inclusive range of to , print Not Weird If is even and in the inclusive range of to , print Weird If is even and greater than      , print Not Weird  Complete the stub code provided in your editor to print whether or not is weird.",
-      difficulty_level: "HARD",
-      marks: 10,
-      sample_input: "3",
-      sample_output: "Weired",
-      test_case: "24",
-      result_test_case: "Not weird",
-      solution_code:
-        "import java.io.*; import java.math.*; import java.security.*; import java.text.*; import java.util.*; import java.util.concurrent.*; import java.util.regex.*;  public class Solution {        private static final Scanner scanner = new Scanner(System.in);      public static void main(String[] args) {         int N = scanner.nextInt();                   scanner.close();     } } ",
-      status: "SOLVED",
-    },
-    {
-      problem_id: 2,
-      contest_id: 1,
-      title: "java-if-else",
-      problem_statement:
-        "In this challenge, we test your knowledge of using if-else conditional statements to automate decision-making processes.",
-      explanation:
-        "Given an integer,  , perform the following conditional actions:      If   is odd, print Weird If is even and in the inclusive range of to , print Not Weird If is even and in the inclusive range of to , print Weird If is even and greater than      , print Not Weird  Complete the stub code provided in your editor to print whether or not is weird.",
-      difficulty_level: "MEDIUM",
-      marks: 10,
-      sample_input: "3",
-      sample_output: "Weired",
-      test_case: "24",
-      result_test_case: "Not weird",
-      solution_code:
-        "import java.io.*; import java.math.*; import java.security.*; import java.text.*; import java.util.*; import java.util.concurrent.*; import java.util.regex.*;  public class Solution {        private static final Scanner scanner = new Scanner(System.in);      public static void main(String[] args) {         int N = scanner.nextInt();                   scanner.close();     } } ",
-      status: "SOLVED",
-    },
-    {
-      problem_id: 3,
-      contest_id: 1,
-      title: "java-if-else",
-      problem_statement:
-        "In this challenge, we test your knowledge of using if-else conditional statements to automate decision-making processes.",
-      explanation:
-        "Given an integer,  , perform the following conditional actions:      If   is odd, print Weird If is even and in the inclusive range of to , print Not Weird If is even and in the inclusive range of to , print Weird If is even and greater than      , print Not Weird  Complete the stub code provided in your editor to print whether or not is weird.",
-      difficulty_level: "EASY",
-      marks: 10,
-      sample_input: "3",
-      sample_output: "Weired",
-      test_case: "24",
-      result_test_case: "Not weird",
-      solution_code:
-        "import java.io.*; import java.math.*; import java.security.*; import java.text.*; import java.util.*; import java.util.concurrent.*; import java.util.regex.*;  public class Solution {        private static final Scanner scanner = new Scanner(System.in);      public static void main(String[] args) {         int N = scanner.nextInt();                   scanner.close();     } } ",
-      status: "UNSOLVED",
-    },
-    {
-      problem_id: 4,
-      contest_id: 1,
-      title: "java-if-else",
-      problem_statement:
-        "In this challenge, we test your knowledge of using if-else conditional statements to automate decision-making processes.",
-      explanation:
-        "Given an integer,  , perform the following conditional actions:      If   is odd, print Weird If is even and in the inclusive range of to , print Not Weird If is even and in the inclusive range of to , print Weird If is even and greater than      , print Not Weird  Complete the stub code provided in your editor to print whether or not is weird.",
-      difficulty_level: "HARD",
-      marks: 10,
-      sample_input: "3",
-      sample_output: "Weired",
-      test_case: "24",
-      result_test_case: "Not weird",
-      solution_code:
-        "import java.io.*; import java.math.*; import java.security.*; import java.text.*; import java.util.*; import java.util.concurrent.*; import java.util.regex.*;  public class Solution {        private static final Scanner scanner = new Scanner(System.in);      public static void main(String[] args) {         int N = scanner.nextInt();                   scanner.close();     } } ",
-      status: "SOLVED",
-    },
-    {
-      problem_id: 5,
-      contest_id: 1,
-      title: "java-if-else",
-      problem_statement:
-        "In this challenge, we test your knowledge of using if-else conditional statements to automate decision-making processes.",
-      explanation:
-        "Given an integer,  , perform the following conditional actions:      If   is odd, print Weird If is even and in the inclusive range of to , print Not Weird If is even and in the inclusive range of to , print Weird If is even and greater than      , print Not Weird  Complete the stub code provided in your editor to print whether or not is weird.",
-      difficulty_level: "HARD",
-      marks: 10,
-      sample_input: "3",
-      sample_output: "Weired",
-      test_case: "24",
-      result_test_case: "Not weird",
-      solution_code:
-        "import java.io.*; import java.math.*; import java.security.*; import java.text.*; import java.util.*; import java.util.concurrent.*; import java.util.regex.*;  public class Solution {        private static final Scanner scanner = new Scanner(System.in);      public static void main(String[] args) {         int N = scanner.nextInt();                   scanner.close();     } } ",
-      status: "UNSOLVED",
-    },
-  ];
-
   const { contest_id } = useParams();
 
-  // Filter problems based on the contest_id
-  const contestProblems = problemsData.filter(
-    (problem) => problem.contest_id.toString() === contest_id
-  );
+  const navigate = useNavigate();
 
-  // Filter problems based on the difficulty_level
-  const filteredProblems = problemsData.filter(
-    (problem) =>
-      filter[problem.difficulty_level.toLowerCase()] &&
-      filter[problem.status.toUpperCase()]
-  );
+  function QuestionPage(problemId) {
+    navigate(`/student-attemptProblem/${problemId}`);
+  }
+
+  const getFromApi = async () => {
+    try {
+      const result = await getProblems(params.contest_id);
+      console.log(result.data);
+      setProblemData(result.data);
+    } catch (error) {
+      setProblemData([]);
+      console.log("from api", error.data);
+    }
+  };
+
+  useEffect(() => {
+    getFromApi();
+  }, []);
+
+  const filteredProblems = problemData.filter((problem) => {
+    const difficultyFilter =
+      (problem.difficultyLevel === "EASY" && filter.easy) ||
+      (problem.difficultyLevel === "MEDIUM" && filter.medium) ||
+      (problem.difficultyLevel === "HARD" && filter.hard);
+  
+    return difficultyFilter;
+  });
 
   return (
     <Container>
@@ -138,17 +64,27 @@ const S_Contest = () => {
           <h2>Problems</h2>
           {filteredProblems.map((problem) => (
             <div
-              key={problem.problem_id}
+              key={problem.problemId}
               className="border p-3 mb-3 rounded"
               style={{ backgroundColor: "lightgrey" }}
             >
               <h3>{problem.title}</h3>
               <h6>Marks: {problem.marks}</h6>
-              <h6>Difficulty: {problem.difficulty_level}</h6>
+              <h6>Difficulty: {problem.difficultyLevel}</h6>
               {problem.status === "SOLVED" ? (
-                <Button variant="outline-success">SOLVED</Button>
+                <Button
+                  variant="outline-success"
+                  onClick={() => QuestionPage(problem.problemId)}
+                >
+                  SOLVED
+                </Button>
               ) : (
-                <Button variant="outline-danger">UNSOLVED</Button>
+                <Button
+                  variant="outline-danger"
+                  onClick={() => QuestionPage(problem.problemId)}
+                >
+                  UNSOLVED
+                </Button>
               )}
               {/* Add more details as needed */}
             </div>
@@ -166,9 +102,7 @@ const S_Contest = () => {
           <Form>
             <h3>Filters: </h3>
             <Form.Group controlId="formDifficulty">
-              <Form.Label style={{ fontWeight: "bold" }}>
-                Difficulty:
-              </Form.Label>
+              <Form.Label style={{ fontWeight: "bold" }}>Difficulty:</Form.Label>
               <Form.Check
                 type="checkbox"
                 label="Easy"
--- Front_End_React/front_end/src/Services/Student_services/Student_APIs.js
+++ Front_End_React/front_end/src/Services/Student_services/Student_APIs.js
@@ -1,16 +1,27 @@
 import axios from "axios";
 
-
-
-export async function loginStudent(data){
-    try{
-
-        const response = await axios.post('http://localhost:9092/student/login',data);
-        return response;
-
-    }catch(error){
-        console.log(error)
-        return error;
-    }
-
+export async function loginStudent(data) {
+  try {
+    const response = await axios.post(
+      "http://localhost:9090/student/login",
+      data
+    );
+    return response;
+  } catch (error) {
+    console.log(error);
+    return error;
+  }
 }
+
+export async function getProblems(contestId){
+  try{
+
+      const response = await axios.get(`http://localhost:9090/contest/${contestId}/problems`);
+      return response;
+
+  }catch(error){
+      console.log(error)
+      return error;
+  }
+
+} 
