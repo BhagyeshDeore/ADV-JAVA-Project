31260929b25a0cfbc756190cf07f6da87892e35d
RushikeshJagdale10 <rushikeshjagdale10@gmail.com>
2024-01-01 13:44:52 +0530

    backend API of admin login created

--- Back_End_SpringBoot/Backend/src/main/java/com/project/dto/AdminLoginStatus.java
+++ Back_End_SpringBoot/Backend/src/main/java/com/project/dto/AdminLoginStatus.java
@@ -2,12 +2,17 @@ package com.project.dto;
 
 public class AdminLoginStatus {
 	
-	 
+	private boolean status; 
 	private String message;
 	private int adminId;
 	
 	 
-	 
+	public boolean isStatus() {
+		return status;
+	}
+	public void setStatus(boolean status) {
+		this.status = status;
+	} 
 	public String getMessage() {
 		return message;
 	}
