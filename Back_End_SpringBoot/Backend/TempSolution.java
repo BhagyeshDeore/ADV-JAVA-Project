e4d5aef8d5eda39c0b0eb041c46ed33043b21f34
BhagyeshDeore <bhagyeshdeore123@gmail.com>
2024-02-20 15:20:18 +0530

    pull

--- Back_End_SpringBoot/Backend/TempSolution.java
+++ Back_End_SpringBoot/Backend/TempSolution.java
@@ -3,7 +3,7 @@ import java.util.Scanner;
     class Solution{
         public static void main(String args[]){
             Scanner sc = new Scanner(System.in);
-               System.out.println("1,2,3,5,5,7"); 
-           
+              System.out.println("1,2,4,4,6,8");  
+            //write your code here
         }
     }
